5d4b712822bec511f7c12576c5b69db091944de0
adampttnmakun3 <bakitmamjolok@gmail.com>
2025-06-16 04:53:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -139,3 +139,8 @@ async def ask_ai(data: NodeData):
 
     except Exception as e:
         return {"error": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
+
